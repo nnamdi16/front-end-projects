432f0f22b6fa184f12e7821186a46c8aee413967
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-16 08:07:32 +0100

    Create  functions for adding  and removing elements from the DOM when a menu is active.

--- job-board/js/index.js
+++ job-board/js/index.js
@@ -1,3 +1,7 @@
+/**
+ *
+ * @param {*} event
+ */
 function menuDisplay(event) {
   let headerTab = document.getElementsByClassName("header-tab");
   for (let index = 0; index < headerTab.length; index++) {
@@ -5,8 +9,33 @@ function menuDisplay(event) {
       "active",
       ""
     );
+    removeActiveMenuImage("header-active");
   }
 
   event.currentTarget.className += " active";
+  document
+    .getElementsByClassName("active")[0]
+    .appendChild(addActiveMenuImage());
+}
 
+/**
+ * Adds div with an image when the menu is active
+ */
+function addActiveMenuImage() {
+  const div = document.createElement("div");
+  div.className = "header-active";
+  div.innerHTML = `<img src="./img/menu-ellipse.svg" />`;
+  console.log("div created");
+  return div;
+}
+
+/**
+ *Removes div when the menu is inactive
+ * @param {String} className
+ */
+function removeActiveMenuImage(className) {
+  let element = document.getElementsByClassName(className);
+  while (element.length > 0) {
+    element[0].parentNode.removeChild(element[0]);
+  }
 }
